c224cc403da0f05ced29c3bcdb44c9d98a63cfe9
TaariqDeveloper <TaariqDeveloper@gmail.com>
2024-12-03 05:57:22 +0300

    gym

---
+++ data/data.js
@@ -0,0 +1,20 @@
+const Users  = [
+    {
+        id : "1",
+        name : "sad",
+        age  : 23,
+    },
+    {
+        id : "2",
+        name : "sa",
+        age  : 21,
+    },
+    {
+        id : "3",
+        name : "sada",
+        age  : 20,
+    },
+];
+
+console.log(Users[2]);
+
